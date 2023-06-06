752a1dfe2c436d4870b811c032e01fa800ea19ab
SUDHEER341 <sudheer601220@gmail.com>
2023-06-06 13:01:30 +0530

    Added : Added custom linkedlist

---
+++ LinkedList.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedList
+{
+    
+
+   public class LinkedList<T>
+    {
+        private Node<T> head;
+
+        private  class Node<T>
+        {
+            public T Data;
+            public Node<T> Next;
+
+            public Node(T data)
+            {
+                Data = data;
+                Next = null;
+            }
+        }
+
+        public void AddFirst(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        public void AddLast(T data)
+        {
+            Node <T> newNode = new Node<T> (data);
+            if(head == null)
+            {
+                head = newNode;
+                return;
+            }
+            Node<T> current =head;
+            while(current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+
+        public void AddAfter(T existingData , T newData)
+        {
+            Node<T> newNode = new Node<T>(newData);
+            Node<T> current = head;
+
+            while(current != null && !current.Data.Equals(existingData))
+             {
+
+                current = current.Next;
+            }
+            if(current != null)
+            {
+                newNode.Next = current.Next;
+                current.Next = newNode;
+            }
+        }
+
+        public void DeleteFirst()
+        {
+            if(head == null)
+            {
+                return;
+            }
+            head = head.Next;
+        }
+
+        public void DeleteLast()
+        {
+            if(head == null || head.Next == null)
+            {
+                head = null;
+                return;
+            }
+
+            Node<T> current = head;
+            while(current.Next.Next != null)
+            {
+                current =current.Next;
+            }
+            current.Next = null;
+        }
+
+        public void DeleteExisting (T existingData)
+        {
+            if(head == null)
+            {
+                return; 
+            }
+
+            // check if head node have the existing data
+
+            if (head.Data.Equals(existingData))
+            {
+                head = head.Next;
+                return;
+            }
+
+            Node<T> current = head;
+            while(current.Next != null)
+            {
+                // check if next node have the existing data
+                if (current.Next.Data.Equals(existingData))
+                {
+                    current.Next = current.Next.Next;
+                    return;
+                }
+                current = current.Next;
+            }
+        }
+        public void PrintList()
+        {
+            Node <T> current = head;
+            while(current != null)
+            {
+                Console.Write(current.Data + "->");
+                current = current.Next;
+            }
+            Console.WriteLine("END");
+        }
+
+        
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CustomLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         LinkedList<int> linkedList = new LinkedList<int>();
+
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(3);
+
+            linkedList.AddLast(4);// 3->2->1->4
+            linkedList.AddAfter(2, 5); //3->2->5->1->4
+            linkedList.DeleteFirst(); // 2->5->1->4
+            linkedList.DeleteLast(); //2->5->1
+            linkedList.DeleteExisting(5); //2->1
+            linkedList.PrintList();
+         }
+    }
+}
